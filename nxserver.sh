024527e15f035fdb3d1ae4dedcb93f6a0cc5badc
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-26 14:01:34 +0100

    Create nxserver.sh

---
+++ nxserver.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Create User and Group:
+groupadd -r $USER -g 433 \
+&& useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
+&& adduser $USER sudo \
+&& mkdir /home/$USER \
+&& chown -R $USER:$USER /home/$USER \
+&& echo $USER':'$PASSWORD | chpasswd
+userdel -r mate
+
+# Set Timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Set System-Language:
+echo $LANG UTF8 > /etc/locale.gen && update-locale LANG=$LANG LANGUAGE
+
+# Start nxserver-software
+/etc/NX/nxserver --startup
+tail -f /usr/NX/var/log/nxserver.log
