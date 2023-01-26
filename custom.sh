7fc477fe6636ec6f443ab30e47e5e7ecc17eb94a
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-26 13:59:46 +0100

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,35 @@
+# Renove Snap-Version of Firefox and Install latest DEB-Version
+# ------------------------------
+
+snap remove firefox
+apt-get remove -y firefox
+add-apt-repository -y ppa:mozillateam/ppa
+echo "Package: *" > /etc/apt/preferences.d/mozilla-firefox
+echo "Pin: release o=LP-PPA-mozillateam" >> /etc/apt/preferences.d/mozilla-firefox
+echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/mozilla-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+apt install -y firefox
+mv /usr/share/applications/firefox.desktop /usr/share/applications/static-firefox.desktop
+
+# Download and install Chromium Webbrowser
+# ----------------------------------------
+apt-get install -y chromium
+
+# Install Seafile-Cloud-Sync-Client
+# ---------------------------------
+
+wget https://linux-clients.seafile.com/seafile.asc -O /usr/share/keyrings/seafile-keyring.asc
+OSRELEASE=$(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d "=" -f2)
+if [ $OSRELEASE == 'jammy'  ]
+then
+ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/seafile-keyring.asc] https://linux-clients.seafile.com/seafile-deb/jammy/ stable main' > /etc/apt/sources.list.d/seafile.list
+fi
+apt update
+apt install -y seafile-gui
+
+
+# Cleanup
+# -------
+apt remove snapd
+rm -Rf /var/cache/apt/archives
+rm -Rf /var/lib/snapd
