1e5d16dd71c3638a2024b84dda27606e8cb694a8
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-29 23:26:04 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -30,6 +30,12 @@ fi
 apt update
 apt install -y seafile-gui
 
+# Install Visual Studio
+# -----------------------
+# To Get the download-link, install the Video-Download-Helper-Plugin for your browser. Navigate to https://code.visualstudio.com.
+# Navigate to the bottom and dowload the arm-64 Version manually. Then use the Video-Download-Helper to get the link and paste it below:
+
+wget -O /tmp/vstudio.deb "https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/code_1.74.3-1673284080_arm64.deb" && apt-get install /tmp/vstudio.deb
 
 # Cleanup
 # -------
