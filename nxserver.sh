4df53096abbd0d0d5ec006cef723285afccd18cc
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-01-30 10:33:50 +0100

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -4,16 +4,8 @@
 groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
-&& mkdir /home/$USER \
-&& chown -R $USER:$USER /home/$USER \
-&& echo $USER':'$PASSWORD | chpasswd
-userdel -r mate
-
-# Set Timezone
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-
-# Set System-Language:
-echo $LANG UTF8 > /etc/locale.gen && update-locale LANG=$LANG LANGUAGE
+&& mkdir /home/$USER
+chown -R $USER:$USER /home/$USER
 
 # Create Customized Panel
 wget -O /home/$USER/vpsrestore.tar.gz "https://github.com/ramirezfx/ubuntu-mate-desktop-arm64/raw/main/vpsrestore.tar.gz"
@@ -22,6 +14,15 @@ rm /home/$USER/vpsrestore.tar.gz
 chown -Rf $USER /home/$USER/vpsrestore
 chgrp -Rf $USER /home/$USER/vpsrestore
 runuser -l $USER -c "/vpsrestore/1restore.sh"
+
+echo $USER':'$PASSWORD | chpasswd
+userdel -r mate
+
+# Set Timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Set System-Language:
+echo $LANG UTF8 > /etc/locale.gen && update-locale LANG=$LANG LANGUAGE
 
 # Start nxserver-software
 /etc/NX/nxserver --startup
