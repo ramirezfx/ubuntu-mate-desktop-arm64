284b466f2cd9287d099e482aef44f072116f61e5
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2023-04-12 14:56:51 +0200

    Update nxserver.sh

--- nxserver.sh
+++ nxserver.sh
@@ -8,11 +8,11 @@ groupadd -r $USER -g 433 \
 chown -R $USER:$USER /home/$USER
 
 # Create Customized Panel
-wget -O /home/$USER/vpsrestore.tar.gz "https://github.com/ramirezfx/ubuntu-mate-desktop-arm64/raw/main/vpsrestore.tar.gz"
-tar xzf /home/$USER/vpsrestore.tar.gz
-rm /home/$USER/vpsrestore.tar.gz
-chown -Rf $USER /home/$USER/vpsrestore
-chgrp -Rf $USER /home/$USER/vpsrestore
+# wget -O /home/$USER/vpsrestore.tar.gz "https://github.com/ramirezfx/ubuntu-mate-desktop-arm64/raw/main/vpsrestore.tar.gz"
+# tar xzf /home/$USER/vpsrestore.tar.gz
+# rm /home/$USER/vpsrestore.tar.gz
+# chown -Rf $USER /home/$USER/vpsrestore
+# chgrp -Rf $USER /home/$USER/vpsrestore
 # runuser -l $USER -c "/vpsrestore/1restore.sh"
 
 echo $USER':'$PASSWORD | chpasswd
@@ -25,5 +25,5 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo $LANG UTF8 > /etc/locale.gen && update-locale LANG=$LANG LANGUAGE
 
 # Start nxserver-software
-service ssh start && /etc/NX/nxserver --startup
+/etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
